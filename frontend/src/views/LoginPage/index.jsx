13e8e1500d61e8741b62401fa564c4fc7e6ee829
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-04-28 17:30:22 -0500

    update sctruture styles LoginPage

--- frontend/src/views/LoginPage/index.jsx
+++ frontend/src/views/LoginPage/index.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+
+// Styles
+import { Container, Form, LineItem, LinkForm, LogoIcon, Title } from "./styles";
 
 // Services
-import loginService from "../services/loginService";
+import loginService from "../../services/loginService";
 
 // Assets
-import LogoSVG from "../assets/logo.svg";
+import LogoSVG from "../../assets/logo.svg";
 
 // Components
-import WaveForm from "../components/common/WaveForm";
-import ButtonGeneric from "../components/common/ButtonGeneric";
-import InputGeneric from "../components/common/InputGeneric";
+import WaveForm from "../../components/common/WaveForm";
+import ButtonGeneric from "../../components/common/ButtonGeneric";
+import InputGeneric from "../../components/common/InputGeneric";
 
 // Constants
 import {
@@ -21,71 +23,7 @@ import {
   INVALID_CREDENTIALS,
   LOGIN_SUCCESSFUL,
   USER_DATA,
-} from "../constants";
-
-// Styled Components
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  position: relative;
-  display: flexbox;
-  justify-content: center;
-  align-items: center;
-  background: linear-gradient(
-      180deg,
-      rgba(87, 105, 170, 0.25) 0%,
-      rgba(35, 39, 42, 0.25) 100%
-    ),
-    #23272a;
-`;
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  min-width: 450px;
-  background: rgba(153, 170, 181, 0.1);
-  border-radius: 35px;
-  padding: 2em 3.2em;
-`;
-
-const Title = styled.h2`
-  font-style: normal;
-  font-weight: 300;
-  font-size: 1.8em;
-  line-height: 48px;
-  color: #fff;
-  text-align: center;
-  margin-bottom: 1em;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const LogoIcon = styled.img`
-  position: absolute;
-  position: absolute;
-  width: 15em;
-  left: 3.75em;
-  top: 2.9375em;
-  cursor: pointer;
-`;
-
-const LineItem = styled.div`
-  display: inline-block;
-  min-width: ${({ widthLine }) => widthLine}; //85px default
-  min-height: 1px;
-  margin-top: 0.2em;
-  margin-bottom: ${({ marginBottom }) =>
-    marginBottom === "" ? "0" : marginBottom};
-  background-color: #fff;
-`;
-
-const LinkForm = styled(Link)`
-  margin: 1em auto;
-  font-size: 1.1em;
-  color: #7289da;
-`; // END Styled Components
+} from "../../constants";
 
 class LoginPage extends React.Component {
   constructor(props) {
