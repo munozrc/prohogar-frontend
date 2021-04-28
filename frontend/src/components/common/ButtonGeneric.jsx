b4cb2766aeadbf8ffaae809c91ff0c24824846cb
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-04-28 18:36:15 -0500

    update new styles  height buttonGeneric

--- frontend/src/components/common/ButtonGeneric.jsx
+++ frontend/src/components/common/ButtonGeneric.jsx
@@ -4,10 +4,9 @@ import styled from "styled-components";
 const Button = styled.button`
   width: 140px;
   max-width: 140px;
-  height: 45px;
+  height: 40px;
   background-color: #7289da;
-  padding: 0.6em 0;
-  font-size: 1em;
+  font-size: 1.1em;
   color: #fff;
   border: none;
   border-radius: 10px;
