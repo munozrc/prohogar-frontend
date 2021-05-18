5cdb8e91792a449ab1196fb006814522397caabc
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-05-18 13:40:53 -0500

    remove imporst never used

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  HashRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 // Normalice Styles Css
 import "normalize.css";
@@ -14,21 +9,11 @@ import "normalize.css";
 import MainPage from "./views/MainPage";
 import LoginPage from "./views/LoginPage";
 import ClientPage from "./views/ClientPage";
-import ProfessionalPage from "./views/ProfessionalPage";
 import NotFound from "./views/NotFound";
 import CreateAccount from "./views/CreateAccount";
 import WelcomePage from "./views/WelcomePage";
 import SelectAccountType from "./views/SelectAccountType";
 
-// Constans
-import {
-  HOME_ROUTE,
-  LOGIN_ROUTE,
-  SELECT_ACCOUNT_ROUTE,
-  CREATE_ACCOUNT_ROUTE,
-  DASHBOARD_ROUTE,
-  WELCOME_ROUTE,
-} from "./constants";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import { PrivateRoute, PublicRoute } from "./components/Routes";
 
