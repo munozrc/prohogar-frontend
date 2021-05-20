4a7aebdb36b28e58ddfacdba045f9178c3c088ae
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-05-20 15:10:38 -0500

    add endpoint URL env

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const URL_SERVER = "http://localhost:3001/api";
+const URL_SERVER = process.env.REACT_APP_URL_API;
+console.log(URL_SERVER);
 
 export default async function loginService({ email, password }) {
   const { data } = await axios.post(`${URL_SERVER}/login`, { email, password });
--- src/services/registerService.js
+++ src/services/registerService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const URL_SERVER = "http://localhost:3001/api";
+const URL_SERVER = process.env.REACT_APP_URL_API;
+
+console.log(URL_SERVER);
 
 export default async function loginService(newUser) {
   const { data } = await axios.post(`${URL_SERVER}/register`, newUser);
