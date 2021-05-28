1bff0b114014d987f0883df667073013ad1681fa
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-05-27 22:15:59 -0500

    styles to CardService

---
+++ src/components/CardService.js
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+export default function CardService(props) {
+  const { title, description } = props;
+  return (
+    <WrapperElement>
+      <HeaderCard>
+        <IconCategory
+          src={
+            "https://s3.amazonaws.com/timbrit-produccion/icono_tapicero_thumb.png"
+          }
+        />
+        <WrapperTextHeader>
+          <TitleCard>{title}</TitleCard>
+          <CategoryText>Tapicero</CategoryText>
+        </WrapperTextHeader>
+      </HeaderCard>
+      <DescriptionArea>{description}</DescriptionArea>
+    </WrapperElement>
+  );
+}
+
+const WrapperElement = styled.div`
+  width: 100%;
+  height: fit-content;
+  min-height: 120px;
+  display: flex;
+  flex-direction: column;
+  background: ${({ theme }) => theme.bgContent};
+  border-radius: 4px;
+  border: solid 1px ${({ theme }) => theme.borderLightColor};
+  padding: 20px;
+
+  @media (max-width: 360px) {
+    padding: 15px;
+  }
+`;
+
+const IconCategory = styled.img`
+  max-width: 50px;
+  max-height: 50px;
+  padding: 5px;
+  border-radius: 50%;
+  background: ${({ theme }) => theme.bgWhite};
+  overflow: hidden;
+`;
+
+const HeaderCard = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const WrapperTextHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  margin-left: 10px;
+`;
+
+const CategoryText = styled.span`
+  font-size: 14px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.labelColor};
+`;
+
+const TitleCard = styled.h4`
+  width: 100%;
+  font-size: 18px;
+  font-weight: 600;
+  text-align: left;
+  color: ${({ theme }) => theme.bgWhite};
+`;
+
+const DescriptionArea = styled.p`
+  width: 100%;
+  font-size: 16px;
+  color: ${({ theme }) => theme.lightColor};
+`;
--- src/pages/ClientDashboard.js
+++ src/pages/ClientDashboard.js
@@ -1,11 +1,21 @@
+import useClient from "../hooks/useClient";
 import GreetingsSection from "../layouts/GreetingsSection";
 import PageDashboard from "../layouts/PageDashboard";
 import loadDataUser from "../utils/loadDataUser";
 import { LINK_CLIENT } from "../settings";
 import { Redirect, Route, Switch } from "react-router";
+import { useEffect } from "react";
+import CardService from "../components/CardService";
+import styled from "styled-components";
 
 export default function ClientDashboard() {
   const { name, photo } = loadDataUser();
+  const { services, getAllServices } = useClient();
+
+  useEffect(() => {
+    getAllServices();
+  }, [getAllServices]);
+
   return (
     <PageDashboard
       photo={photo}
@@ -18,10 +28,40 @@ export default function ClientDashboard() {
           <GreetingsSection name={name} />
         </Route>
         <Route exact path={LINK_CLIENT[1].path}>
-          <h1>Servicios</h1>
+          <ServicesTab>
+            <TitleTab>Mis servicios</TitleTab>
+            {services.map((service) => (
+              <CardService
+                key={service.id}
+                title={service.title}
+                description={service.description}
+              />
+            ))}
+          </ServicesTab>
         </Route>
         <Route render={() => <Redirect to={"/dashboard"} />} />
       </Switch>
     </PageDashboard>
   );
 }
+
+const ServicesTab = styled.div`
+  width: 100%;
+  padding: 20px;
+
+  @media (max-width: 360px) {
+    padding: 15px;
+  }
+`;
+
+const TitleTab = styled.h2`
+  color: ${({ theme }) => theme.lightColor};
+  font-weight: 700;
+  margin-bottom: 20px;
+  padding-bottom: 3px;
+  border-bottom: solid 1.5px ${({ theme }) => theme.lightColor};
+
+  @media (max-width: 360px) {
+    margin-bottom: 15px;
+  }
+`;
