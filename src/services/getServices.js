8dd4af1f59b88af4aa2466e62462115aa38de2d9
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-05-27 21:08:32 -0500

    remove console log

--- src/services/getServices.js
+++ src/services/getServices.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import { URL_SERVER } from "../settings";
 
 export async function getServices({ token }) {
-  console.log({ token });
   const { data } = await axios.get(`${URL_SERVER}/services`, {
     headers: { Authorization: `Bearer ${token}` },
   });
