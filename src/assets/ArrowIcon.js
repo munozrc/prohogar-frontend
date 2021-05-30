fbeb0139a7eb0443380c3b24293ab3ad06b65411
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-05-30 00:14:08 -0500

    new icon for ui

---
+++ src/assets/ArrowIcon.js
@@ -0,0 +1,22 @@
+export default function ArrowIcon(props) {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth={0}
+      viewBox="0 0 512 512"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fill="none"
+        strokeLinecap="square"
+        strokeMiterlimit={10}
+        strokeWidth={48}
+        d="M184 112l144 144-144 144"
+      />
+    </svg>
+  );
+}
