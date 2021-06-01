adee1d11bc4fc9e8ea8dbcce1d0e0a236ec97588
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-06-01 17:31:54 -0500

    create componen tab greeting professional

---
+++ src/pages/ProDashboard/GreetingTab.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import loadDataUser from "../../utils/loadDataUser";
+
+export default function GreetingTab() {
+  const { name } = loadDataUser();
+  return (
+    <SectionElement>
+      <NameUser>{`Hola ${name}`}</NameUser>
+      <GreetingsDisplay>Bienvenido de nuevo 👋</GreetingsDisplay>
+    </SectionElement>
+  );
+}
+
+const SectionElement = styled.section`
+  width: 100%;
+  height: fit-content;
+  padding: 50px;
+`;
+
+const NameUser = styled.h3`
+  color: ${({ theme }) => theme.bgWhite};
+  font-size: 24px;
+  font-weight: 400;
+`;
+const GreetingsDisplay = styled.h4`
+  color: ${({ theme }) => theme.bgWhite};
+  font-size: 42px;
+`;
--- src/pages/ProDashboard/index.js
+++ src/pages/ProDashboard/index.js
@@ -8,17 +8,15 @@ import loadDataUser from "../../utils/loadDataUser";
 import { LINK_PRO } from "../../settings";
 
 // Local Custom Components
-import GreetingsSection from "../../layouts/GreetingsSection";
 import RequestsTab from "./RequestsTab";
+import GreetingTab from "./GreetingTab";
 
 export default function ProDashboard() {
   const { name, photo, category } = loadDataUser();
   return (
     <PageDashboard photo={photo} name={name} type={category} links={LINK_PRO}>
       <Switch>
-        <Route exact path={"/dashboard"}>
-          <GreetingsSection name={name} />
-        </Route>
+        <Route exact path={"/dashboard"} component={GreetingTab} />
         <Route exact path={"/dashboard/requests"} component={RequestsTab} />
         <Route render={() => <Redirect to={"/dashboard"} />} />
       </Switch>
