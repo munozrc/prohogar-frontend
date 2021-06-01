1c794128785685147a0d84bec7701cb1449315e7
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-06-01 14:32:13 -0500

    data api reset client

--- src/components/CardRequest/index.js
+++ src/components/CardRequest/index.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from "react";
+import { useCallback } from "react";
 import styled from "styled-components";
 
 // Custom Components
@@ -13,16 +13,10 @@ import loadDataUser from "../../utils/loadDataUser";
 
 // Assets
 import MoreOptionsIcon from "../../assets/MoreOptionsIcon";
-import useProfessional from "../../hooks/useProfessional";
 
 export default function CardRequest(props) {
   const { title, description, location, offers, professional, client } = props;
-  const { clientData, getDataClient } = useProfessional();
   const { name, id } = loadDataUser();
-
-  useEffect(() => {
-    getDataClient({ id: client });
-  }, [client, getDataClient]);
 
   const RenderButtonOffer = useCallback(() => {
     const proFind = offers.find((pro) => pro.id === id && pro.acceptRequest);
@@ -32,30 +26,26 @@ export default function CardRequest(props) {
   }, [id, offers]);
 
   return (
-    <>
-      {clientData && professional === null && (
-        <Wrapper>
-          <Header>
-            <UserImageWrapper>
-              <UserImage src={clientData.photo} />
-            </UserImageWrapper>
-            <WrapperTextHeader>
-              <TitleCard>{clientData.name}</TitleCard>
-              <SubTitle>Cliente</SubTitle>
-            </WrapperTextHeader>
-            <OptionsButton />
-          </Header>
-          <Details
-            title={title}
-            location={location}
-            description={description}
-            professional={professional}
-          />
-          <Offers offers={offers} professionalName={name} />
-          <RenderButtonOffer />
-        </Wrapper>
-      )}
-    </>
+    <Wrapper>
+      <Header>
+        <UserImageWrapper>
+          <UserImage src={client.photo} />
+        </UserImageWrapper>
+        <WrapperTextHeader>
+          <TitleCard>{client.name}</TitleCard>
+          <SubTitle>Cliente</SubTitle>
+        </WrapperTextHeader>
+        <OptionsButton />
+      </Header>
+      <Details
+        title={title}
+        location={location}
+        description={description}
+        professional={professional}
+      />
+      <Offers offers={offers} professionalName={name} />
+      <RenderButtonOffer />
+    </Wrapper>
   );
 }
 
--- src/hooks/useProfessional.js
+++ src/hooks/useProfessional.js
@@ -1,10 +1,8 @@
 import { useCallback, useState } from "react";
 import { getRequests } from "../services/getRequests";
-import getSingleUser from "../services/getSingleUser";
 
 export default function useProfessional() {
   const [requests, setRequests] = useState([]);
-  const [clientData, setClientData] = useState(null);
 
   const getAllRequests = useCallback(() => {
     getRequests().then((response) => {
@@ -13,18 +11,8 @@ export default function useProfessional() {
     });
   }, []);
 
-  const getDataClient = useCallback(({ id }) => {
-    getSingleUser({ id }).then((response) => {
-      if (response.message === "SUCCESSFUL_QUERY") {
-        setClientData(() => response.data);
-      }
-    });
-  }, []);
-
   return {
     requests,
-    clientData,
     getAllRequests,
-    getDataClient,
   };
 }
